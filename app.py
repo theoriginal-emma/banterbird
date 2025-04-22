f7b6cdf0ce72ba37226070f687238608f9444cd1
theoriginal-emma <engarrison@icloud.com>
2025-04-22 13:45:56 -0500

    Post almost works (needs refresh)

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, jsonify
+from flask import Flask, render_template, jsonify, request
 import json
 
 app = Flask(__name__)
@@ -12,6 +12,19 @@ def get_posts():
     with open('posts.json', 'r') as file:
         posts = json.load(file)
     return jsonify(posts)
+
+@app.route('/api/add_post', methods=['POST'])
+def add_post():
+    new_post = request.get_json()
+    
+    with open('posts.json', 'r') as file:
+        posts = json.load(file)
+    posts.insert(0, new_post)
+
+    with open('posts.json', 'w') as file:
+        json.dump(posts, file, indent = 4)
+
+    return jsonify({"status": "success"}), 201
 
 if __name__ == '__main__':
     app.run(debug=True)
