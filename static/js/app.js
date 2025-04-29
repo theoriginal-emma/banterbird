6d2950710f6ac5e93eef3535bb6e42432f024808
theoriginal-emma <engarrison@icloud.com>
2025-04-29 13:42:23 -0500

    Banterbird login page

--- static/js/app.js
+++ static/js/app.js
@@ -1,4 +1,7 @@
-const username = "admin";
+let username = localStorage.getItem("username");
+if (!username){
+  window.location.href = "/login";
+}
 
 function renderPost(post, isNew = false) {
     const template = document
@@ -34,6 +37,7 @@ async function submitPost() {
 
 window.onload = async () => {
     try{
+        document.getElementById("username").innerText = username;
         const response = await fetch("/api/posts");
         const posts = await response.json();
         posts.forEach((post) => renderPost(post));
