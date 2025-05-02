ac310f13ff5c7e4c59bdef23e29af160f3150d33
theoriginal-emma <engarrison@icloud.com>
2025-05-02 10:59:41 -0500

    Anyone who goes to website can see the feed

--- static/js/app.js
+++ static/js/app.js
@@ -45,3 +45,13 @@ window.onload = async () => {
         console.error("Error fetching posts:", error);
     }
 };
+
+setInterval(async () => {
+  try{
+    const response = await fetch("/api/posts");
+    const posts = await response.json();
+    posts.forEach((post) => renderPost(post));
+  }  catch (error) {
+    console.error("Error fetching posts:", error);
+  }
+}, 5000);
